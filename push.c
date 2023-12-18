fbcf93f726838d57ba300de4c8ff0cc626b9cd90
JoelALukwe <alukwe7@gmail.com>
2023-12-18 13:41:57 -0800

    0x19. C - Stacks, Queues - LIFO, FIFO

--- push.c
+++ push.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 /**
 * f_push - add node to the stack
-* @head: stack head
+* @kichwa: stack head
 * @counter: line_number
 * Return: no return
 */
-void f_push(stack_t **head, unsigned int counter)
+void f_push(stack_t **kichwa, unsigned int counter)
 {
 int n, j = 0, flag = 0;
 
@@ -21,17 +21,17 @@ if (flag == 1)
 { fprintf(stderr, "L%d: usage: push integer\n", counter);
 fclose(bus.file);
 free(bus.content);
-free_stack(*head);
+free_stack(*kichwa);
 exit(EXIT_FAILURE); }}
 else
 { fprintf(stderr, "L%d: usage: push integer\n", counter);
 fclose(bus.file);
 free(bus.content);
-free_stack(*head);
+free_stack(*kichwa);
 exit(EXIT_FAILURE); }
 n = atoi(bus.arg);
 if (bus.lifi == 0)
-addnode(head, n);
+addnode(kichwa, n);
 else
 addqueue(head, n);
 }
